bd604ca5fbb5a3d047d832903a65c5967c807959
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-21 17:29:45 +0200

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -13,8 +13,8 @@ params = yaml.safe_load(open('params.yaml'))['train']
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
-model = RandomForestRegressor()
 model = LinearRegression(normalize=True)
+#model = RandomForestRegressor()
 
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
