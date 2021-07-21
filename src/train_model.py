008514a9e7f69255aaf8188e95aa56b213e48e70
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-21 14:27:47 +0200

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -12,9 +12,7 @@ params = yaml.safe_load(open('params.yaml'))['train']
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
-model = RandomForestRegressor(
-    n_estimators=params["n_estimators"], max_depth=params["max_depth"], random_state=params["seed"], ccp_alpha=params["ccp_alpha"]
-)
+model = RandomForestRegressor( n_estimators=params["n_estimators"], max_depth=params["max_depth"], random_state=params["seed"], ccp_alpha=params["ccp_alpha"])
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
