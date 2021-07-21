e0d68c56f60f94a65ccf2831bfcc535d9a068bff
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-21 16:34:49 +0200

    Update prepare.py

--- src/prepare.py
+++ src/prepare.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import yaml
@@ -12,15 +12,16 @@ Config.DATASET_PATH.mkdir(parents=True, exist_ok=True)
 
 params = yaml.safe_load(open('params.yaml'))['prepare']
 
-load_diabetes(str(Config.ORIGINAL_DATASET_FILE_PATH))
-data = load_diabetes()
+fetch_california_housing(str(Config.ORIGINAL_DATASET_FILE_PATH))
+data = fetch_california_housing()
 df = pd.DataFrame(data=data['data'], columns = data['feature_names'])
-df['MEDV'] = data.target
+df['Target'] = data.target
 
-df.to_csv(str(Config.ORIGINAL_DATASET_FILE_PATH / "diabetes.csv"), sep = ',', index = False)
+df.to_csv(str(Config.ORIGINAL_DATASET_FILE_PATH / "cali.csv"), sep = ',', index = False)
 
 
 df_train, df_test = train_test_split( df, test_size=params["split"], random_state=params["seed"] )
 
 df_train.to_csv(str(Config.DATASET_PATH / "train.csv"), index=None)
 df_test.to_csv(str(Config.DATASET_PATH / "test.csv"), index=None)
+
